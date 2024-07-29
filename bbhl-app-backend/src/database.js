bcf782091a71f747bb12c8bdafff4802e4d2cf04
Eric Spensieri <espensieri@hotmail.com>
2024-07-29 10:45:43 -0400

    Testing with log

--- bbhl-app-backend/src/database.js
+++ bbhl-app-backend/src/database.js
@@ -9,7 +9,7 @@ dotenv.config();
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+console.log(process.env);
 // Create a connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,

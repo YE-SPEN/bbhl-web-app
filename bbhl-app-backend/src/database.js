ad7b4f29355ff25993ecc44f09b0a5c63dbdc425
Eric Spensieri <espensieri@hotmail.com>
2024-07-30 11:55:46 -0400

    Process env removed again

--- bbhl-app-backend/src/database.js
+++ bbhl-app-backend/src/database.js
@@ -9,7 +9,6 @@ dotenv.config();
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(process.env);
 
 // Create a connection pool
 const pool = mysql.createPool({

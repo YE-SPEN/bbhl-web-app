2b6c4be228cf7ae93dbceec2323cf4b80632980e
Eric Spensieri <espensieri@hotmail.com>
2024-08-01 11:04:34 -0400

    Uploading deck of cards

---
+++ bbhl-app/src/app/blackjack/blackjack.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlackjackComponent } from './blackjack.component';
+
+describe('BlackjackComponent', () => {
+  let component: BlackjackComponent;
+  let fixture: ComponentFixture<BlackjackComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BlackjackComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BlackjackComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ bbhl-app/src/app/blackjack/blackjack.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blackjack',
+  standalone: true,
+  imports: [],
+  templateUrl: './blackjack.component.html',
+  styleUrl: './blackjack.component.css'
+})
+export class BlackjackComponent {
+
+}

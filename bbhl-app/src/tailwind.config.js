6ce1a8d8d73a817bad1b9f991effc335947ac373
Eric Spensieri <espensieri@hotmail.com>
2024-08-08 17:02:42 -0400

    First Tailwind configuration attempt

---
+++ bbhl-app/src/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    'app/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+  important: true,
+  
+}

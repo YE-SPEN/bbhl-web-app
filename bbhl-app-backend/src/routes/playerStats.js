5f0225013729b56a911381757fe7a5bdc6bc3d70
Eric Spensieri <espensieri@hotmail.com>
2024-08-19 17:29:41 -0400

    Goalie Stats added to player route

--- bbhl-app-backend/src/routes/playerStats.js
+++ bbhl-app-backend/src/routes/playerStats.js
@@ -6,7 +6,7 @@ export const playerStatsRoute = {
     handler: async (req, h) => {
         const year = req.query.year;
 
-        const { results } = await db.query(
+        const { results: playerStats } = await db.query(
             `SELECT ps.*, p.position, p.id, t.logo FROM player_stats ps, players p, teams t
             WHERE ps.season = ?
                 AND ps.name = p.name
@@ -14,6 +14,18 @@ export const playerStatsRoute = {
             ORDER BY ps.points DESC, ps.name DESC`,
             [year],
         );
-        return results;
+        const players = playerStats;
+
+        const { results: goalieStats } = await db.query(
+            `SELECT gs.*, p.position, p.id, t.logo FROM goalie_stats gs, players p, teams t
+            WHERE gs.season = ?
+                AND gs.name = p.name
+                AND gs.team = t.name
+            ORDER BY gs.wins DESC, gs.name DESC`,
+            [year],
+        );
+        const goalies = goalieStats;
+
+        return { players, goalies };
     }
 }
